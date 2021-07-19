d5811f7250bf320d132fe88f93fb68bf6794904d
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-19 22:09:16 +0200

    018 Taking Decisions_ if _ else Statements

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -147,8 +147,9 @@ const BMIJohn = massJohn / (heightJohn * heightJohn);
 const markHigherBMI = BMIMark > BMIJohn;
 
 console.log(BMIMark, BMIJohn, markHigherBMI);
-*/
 
+////////////////////////////////////
+// Strings and Template Literals
 const firstName = 'Jonas';
 const job = 'teacher';
 const birthYear = 1991;
@@ -169,3 +170,23 @@ lines');
 console.log(`String
 multiple
 lines`);
+*/
+
+const age = 15;
+
+if (age >= 18) {
+    console.log('Sarah can start driving license 🚗')
+} else {
+    const yearsLeft = 18 - age
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`)
+};
+
+const birthYear = 2012;
+
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+};
+
+console.log(century)
