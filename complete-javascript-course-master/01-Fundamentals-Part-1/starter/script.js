267a1fb4eb12dc778ac882c42064287db41d22a1
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-20 19:00:47 +0200

    021 Truthy and Falsy Values

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -230,7 +230,6 @@ if (BMIMark > BMIJohn) {
 
 ////////////////////////////////////
 // Type Conversion and Coercion
-*/
 
 // type conversion
 const inputYear = '1991';
@@ -248,6 +247,32 @@ console.log('23' - '10' - 3);
 console.log('23' / '2');
 console.log('23' > '18');
 
-let n = '1' + 1;
+let n = '1' + 1; // '11'
 n = n - 1;
 console.log(n);
+
+////////////////////////////////////
+// Truthy and Falsy Values
+*/
+
+// 5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 100;
+if (money) {
+    console.log("Don't spend it all ;)");
+} else {
+    console.log('You should get a job!');
+}
+
+let height = 0;
+if (height) {
+    console.log('YAY! Height is defined');
+} else {
+    console.log('Height is UNDEFINED');
+}
