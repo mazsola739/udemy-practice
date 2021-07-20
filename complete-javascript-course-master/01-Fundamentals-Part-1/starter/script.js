22c2851c58af86e7eb80f9acf8c5e86509383dbc
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-20 18:45:44 +0200

    020 Type Conversion and Coercion

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -206,8 +206,8 @@ Use the BMI example from Challenge #1, and the code you already wrote, and impro
 HINT: Use an if/else statement 😉
 
 GOOD LUCK 😀
-*/
 
+/*
 const massMark = 78;
 const heightMark = 1.69;
 const massJohn = 92;
@@ -220,10 +220,34 @@ const heightJohn = 1.95;
 
 const BMIMark = massMark / heightMark ** 2;
 const BMIJohn = massJohn / (heightJohn * heightJohn);
-console.log(BMIMark, BMIJohn)
+console.log(BMIMark, BMIJohn);
 
 if (BMIMark > BMIJohn) {
-    console.log(`Mark's BMI (${BMIMark}) is higher than John's! (${BMIJohn})`)
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
 } else {
-    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`)
+  console.log(`John's BMI (${BMIJohn}) is higher than Marks's (${BMIMark})!`)
 }
+
+////////////////////////////////////
+// Type Conversion and Coercion
+*/
+
+// type conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' / '2');
+console.log('23' > '18');
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n);
