f79c375a680c99996cf5f07c4fb1bf4f5e993adf
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-21 09:48:55 +0200

    027 Statements and Expressions

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -370,10 +370,9 @@ if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
 } else {
   console.log('No one wins the trophy 😭');
 }
-*/
+
 ////////////////////////////////////
 // The switch Statement
-
 const day = 'friday';
 
 switch (day) {
@@ -413,3 +412,17 @@ if (day === 'monday') {
 } else {
   console.log('Not a valid day!');
 }
+*/
+
+////////////////////////////////////
+// Statements and Expressions
+3 + 4
+1991
+true && false && !false
+
+if (23 > 10) {
+  const str = '23 is bigger';
+}
+
+const me = 'Jonas';
+console.log(`I'm ${2037 - 1991} years old ${me}`);
