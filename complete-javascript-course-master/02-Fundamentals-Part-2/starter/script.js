1861d8fee6983a788b4e7075fabb9512d6bd2e69
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-21 18:04:38 +0200

    037 Reviewing Functions

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -71,18 +71,43 @@ const yearsUntilRetirement = (birthYeah, firstName) => {
 }
 
 console.log(yearsUntilRetirement(1991, 'Jonas')); console.log(yearsUntilRetirement(1980, 'Bob'));
-*/
 
+
+///////////////////////////////////////
+// Functions Calling Other Functions
 function cutFruitPieces(fruit) {
-    return fruit * 4;
+  return fruit * 4;
 }
 
 function fruitProcessor(apples, oranges) {
-    const applePieces = cutFruitPieces(apples);
-    const orangePieces = cutFruitPieces(oranges);
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
 
-    const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} piece of orange.`;
-    return juice;
+  const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} pieces of orange.`;
+  return juice;
+}
+console.log(fruitProcessor(2, 3));
+*/
+
+const calcAge = function (birthYeah) {
+    return 2037 - birthYeah;
 }
 
-console.log(fruitProcessor(2, 3));
+function yearsUntilRetirement(birthYeah, firstName) {
+    const age = calcAge(birthYeah);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired 🎉`)
+        return -1;
+    }
+
+
+    //return ;
+}
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
