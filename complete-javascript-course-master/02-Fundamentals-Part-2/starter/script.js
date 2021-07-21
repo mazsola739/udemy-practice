c0b253f59232ce0555ca1458f6ba8854de83a99f
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-21 16:54:06 +0200

    034 Function Declarations vs. Expressions

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -11,10 +11,12 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 // const interface = 'Audio';
 // const private = 534;
-*/
 
+
+///////////////////////////////////////
+// Functions
 function logger() {
-    console.log('My name is Jonas');
+  console.log('My name is Jonas');
 }
 
 // calling / running / invoking function
@@ -23,8 +25,8 @@ logger();
 logger();
 
 function fruitProcessor(apples, oranges) {
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-    return juice;
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
 }
 
 const appleJuice = fruitProcessor(5, 0);
@@ -33,4 +35,20 @@ console.log(appleJuice);
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
-const num = ('23');
+const num = Number('23');
+
+*/
+// Function declaration
+function calcAge1(birthYeah) {
+    return 2037 - birthYeah;
+}
+const age1 = calcAge1(1991);
+
+
+// Function expression
+const calcAge2 = function (birthYeah) {
+    return 2037 - birthYeah;
+}
+const age2 = calcAge2(1991);
+
+console.log(age1, age2)
