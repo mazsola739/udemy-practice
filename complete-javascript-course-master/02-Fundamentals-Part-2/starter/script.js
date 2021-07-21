568181486ff47328642387b27d1deedf51c4f43a
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-21 11:49:43 +0200

    033 Functions

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/*
+///////////////////////////////////////
+// Activating Strict Mode
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -8,3 +11,26 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 // const interface = 'Audio';
 // const private = 534;
+*/
+
+function logger() {
+    console.log('My name is Jonas');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = ('23');
