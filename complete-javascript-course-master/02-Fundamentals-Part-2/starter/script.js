01f8c5beaa15ebc791b91414048894b4dadbcc47
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-21 23:36:23 +0200

    040 Basic Array Operations (Methods)

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -162,14 +162,16 @@ scoreDolphins = calcAverage(85, 54, 41);
 scoreKoalas = calcAverage(23, 34, 27);
 console.log(scoreDolphins, scoreKoalas);
 checkWinner(scoreDolphins, scoreKoalas);
-*/
 
-const friend1 = "Michael";
-const friend2 = "Steven";
-const friend3 = "Peter";
+
+///////////////////////////////////////
+// Introduction to Arrays
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
 
 const friends = ['Michael', 'Steven', 'Peter'];
-console.log(friends)
+console.log(friends);
 
 const y = new Array(1991, 1984, 2008, 2020);
 
@@ -181,9 +183,9 @@ console.log(friends[friends.length - 1]);
 
 friends[2] = 'Jay';
 console.log(friends);
-// friends = ['Bob', 'Alice'];
+// friends = ['Bob', 'Alice']
 
-const firstName = 'Jonas'
+const firstName = 'Jonas';
 const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
 console.log(jonas);
 console.log(jonas.length);
@@ -192,12 +194,44 @@ console.log(jonas.length);
 const calcAge = function (birthYeah) {
   return 2037 - birthYeah;
 }
-const years = [1990, 1967, 2002, 2010, 2018]
+const years = [1990, 1967, 2002, 2010, 2018];
+
 const age1 = calcAge(years[0]);
 const age2 = calcAge(years[1]);
 const age3 = calcAge(years[years.length - 1]);
-
-console.log(age1, age2, age3)
+console.log(age1, age2, age3);
 
 const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
 console.log(ages);
+*/
+
+const friends = ['Michael', 'Steven', 'Peter'];
+
+// Add elements
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength)
+
+friends.unshift('John');
+console.log(friends);
+
+// Remove elements
+friends.pop(); // Last
+const popped = friends.pop();
+console.log(popped)
+console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(23)
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+
+if (friends.includes('Steven')) {
+  console.log('You have a friend called Steven')
+}
