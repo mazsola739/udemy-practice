af9728343ba90206505524c842fe6000f0c60827
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-22 19:01:16 +0200

    049 The while Loop

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -488,8 +488,10 @@ for (let i = 0; i < jonas.length; i++) {
 
   console.log(jonas[i], typeof jonas[i]);
 }
-*/
 
+
+///////////////////////////////////////
+// Looping Backwards and Loops in Loops
 const jonas = [
   'Jonas',
   'Schmedtmann',
@@ -510,6 +512,26 @@ for (let exercise = 1; exercise < 4; exercise++) {
   console.log(`-------- Starting exercise ${exercise}`);
 
   for (let rep = 1; rep < 6; rep++) {
-    console.log(`Exercise: ${exercise} Lifting weights repetition ${rep} 🏋️‍♀️`);
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
   }
+}
+*/
+
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
+// }
+
+let rep = 1;
+while (rep <= 10) {
+  // console.log(`WHILE: Lifting weights repetition ${rep} 🏋️‍♀️`);
+  rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log('Loops is about end...')
 }
