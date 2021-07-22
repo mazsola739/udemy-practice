b98ffc6c6e4f4eefe3bfbbf5d4a9d19cf06bc8cf
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-22 20:30:56 +0200

    056 Installing Node.js and Setting Up a Dev Environment

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -2,8 +2,6 @@
 'use strict';
 
 const x = 23;
-if (x === 23) console.log(23);
 
-const calcAge = birthYear = 2037 - birthYear;
-
-console.log(x);
+const calcAge = birthYear => 2037 - birthYear;
+console.log(calcAge(1991));
