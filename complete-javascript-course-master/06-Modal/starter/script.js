2882703dd776a61349e73a7329c7771de6bc90c4
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-23 13:47:21 +0200

    080 Working With Classes

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -2,8 +2,22 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnsCloseModal = document.querySelector('.close-modul');
+const btnsCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
-console.log(btnsShowModal);
 
-for (let i = 0; i < btnsShowModal.length; i++) console.log(btnsShowModal[i].textContent);
+const showModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsShowModal.length; i++)
+    btnsShowModal[i].addEventListener('click', showModal);
+
+btnsCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
