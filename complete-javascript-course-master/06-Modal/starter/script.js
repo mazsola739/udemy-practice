7dd8935bf1c305efe6819e0cdcbd49bd6be22c6c
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-23 13:14:41 +0200

    079 PROJECT #2_ Modal Window

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsCloseModal = document.querySelector('.close-modul');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+console.log(btnsShowModal);
+
+for (let i = 0; i < btnsShowModal.length; i++) console.log(btnsShowModal[i].textContent);
