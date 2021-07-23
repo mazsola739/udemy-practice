aec65c5861748b44341f4bb4f35578de24f02198
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-23 11:33:52 +0200

    076 Coding Challenge #1

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -11,10 +11,9 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
@@ -27,9 +26,11 @@ document.querySelector('.check').addEventListener('click', function () {
         // When player wins
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = '🎉 Correct Number!';
+        document.querySelector('.number').textContent = secretNumber;
+
 
         document.querySelector('body').style.backgroundColor = '#60b347';
-        document.querySelector('.number').style.widht = '30rem';
+        document.querySelector('.number').style.width = '30rem';
 
         // When guess is too high
     } else if (guess > secretNumber) {
@@ -56,3 +57,29 @@ document.querySelector('.check').addEventListener('click', function () {
     }
 })
 
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+    document.querySelector('.message').textContent = 'Start guessing...'
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+
+    document.querySelector('body').style.backgroundColor = '#222'
+    document.querySelector('.number').style.width = '15rem';
+}
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+
+GOOD LUCK 😀
+*/
