111d4b84b1b0380420b4410420b016d76b42b6e2
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-25 21:32:38 +0200

    097 The this Keyword in Practice

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -40,7 +40,10 @@ const firstName = 'Jonas';
 calcAge(1991);
 // console.log(age);
 // printAge();
-*/
+
+
+///////////////////////////////////////
+// Hoisting and TDZ in Practice
 
 // Variables
 console.log(me);
@@ -58,11 +61,11 @@ console.log(addArrow);
 // console.log(addArrow(2, 3));
 
 function addDecl(a, b) {
-    return a + b;
+  return a + b;
 }
 
 const addExpr = function (a, b) {
-    return a + b;
+  return a + b;
 };
 
 var addArrow = (a, b) => a + b;
@@ -74,7 +77,7 @@ if (!numProducts) deleteShoppingCart();
 var numProducts = 10;
 
 function deleteShoppingCart() {
-    console.log('All products deleted!');
+  console.log('All products deleted!');
 }
 
 var x = 1;
@@ -84,3 +87,39 @@ const z = 3;
 console.log(x === window.x);
 console.log(y === window.y);
 console.log(z === window.z);
+*/
+
+///////////////////////////////////////
+// The this Keyword in Practice
+console.log(this);
+
+const calcAge = function (birthYear) {
+    console.log(2037 - birthYear);
+    console.log(this);
+};
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+    console.log(2037 - birthYear);
+    console.log(this);
+};
+calcAgeArrow(1980);
+
+const jonas = {
+    year: 1991,
+    calcAge: function () {
+        console.log(this);
+        console.log(2037 - this.year);
+    },
+};
+jonas.calcAge();
+
+const matilda = {
+    year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f();
