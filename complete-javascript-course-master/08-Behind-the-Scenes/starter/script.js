34b68646af5e4903246d4a05497416290d4f4ed0
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-25 20:29:19 +0200

    095 Hoisting and TDZ in Practice

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,41 +1,86 @@
 'use strict';
 
+///////////////////////////////////////
+// Scoping in Practice
+
+/*
 function calcAge(birthYear) {
-    const age = 2037 - birthYear;
+  const age = 2037 - birthYear;
 
-    function printAge() {
-        let output = `${firstName} are ${age}, born in ${birthYear}`;
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
 
-        console.log(output);
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      // Creating NEW variable with same name as outer scope's variable
+      const firstName = 'Steven';
 
-        if (birthYear >= 1981 && birthYear <= 1996) {
-            var millenial = true;
-            // Creating NEW variable with same name as outer scope's variable
-            const firstName = 'Steven';
+      // Reasssigning outer scope's variable
+      output = 'NEW OUTPUT!';
 
-            // Reassigning outer scope's variable
-            output = 'NEW OUTPUT';
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
 
-            const str = `Oh, and you're a millenial, ${firstName}`;
-            console.log(str);
-
-            function add(a, b) {
-                return a + b;
-            }
-
-        }
-
-        // console.log(str);
-        console.log(millenial);
-        // console.log(add(2, 3));
-        console.log(output);
+      function add(a, b) {
+        return a + b;
+      }
     }
-    printAge();
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(2, 3));
+    console.log(output);
+  }
+  printAge();
 
-    return age;
+  return age;
 }
 
 const firstName = 'Jonas';
 calcAge(1991);
 // console.log(age);
 // printAge();
+*/
+
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+    return a + b;
+}
+
+const addExpr = function (a, b) {
+    return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(undefined);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+    console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
