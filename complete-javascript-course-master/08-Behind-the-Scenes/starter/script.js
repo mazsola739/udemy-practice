028083362599376a25f8726f305d90e9de93f0ad
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-25 22:00:50 +0200

    099 Primitives vs. Objects (Primitive vs. Reference Types)

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -94,28 +94,28 @@ console.log(z === window.z);
 console.log(this);
 
 const calcAge = function (birthYear) {
-    console.log(2037 - birthYear);
-    console.log(this);
+  console.log(2037 - birthYear);
+  console.log(this);
 };
 calcAge(1991);
 
 const calcAgeArrow = birthYear => {
-    console.log(2037 - birthYear);
-    console.log(this);
+  console.log(2037 - birthYear);
+  console.log(this);
 };
 calcAgeArrow(1980);
 
 const jonas = {
-    year: 1991,
-    calcAge: function () {
-        console.log(this);
-        console.log(2037 - this.year);
-    },
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
 };
 jonas.calcAge();
 
 const matilda = {
-    year: 2017,
+  year: 2017,
 };
 
 matilda.calcAge = jonas.calcAge;
@@ -123,52 +123,69 @@ matilda.calcAge();
 
 const f = jonas.calcAge;
 f();
-*/
 
-// var firstName = 'Matilda'
+
+///////////////////////////////////////
+// Regular Functions vs. Arrow Functions
+// var firstName = 'Matilda';
 
 const jonas = {
-    firstName: 'Jonas',
-    year: 1991,
-    calcAge: function () {
-        // console.log(this);
-        console.log(2037 - this.year);
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    // console.log(this);
+    console.log(2037 - this.year);
 
-        // Solution 1
-        // const self = this; // self or that
-        // const isMillenial = function () {
-        //     console.log(self);
-        //     console.log(self.year >= 1981 && self.year <= 1996);
-        // };
+    // Solution 1
+    // const self = this; // self or that
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
 
-        // Solution 2
-        const isMillenial = () => {
-            console.log(this);
-            console.log(this.year >= 1981 && this.year <= 1996);
-        };
-        isMillenial();
-    },
+    // Solution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
 
-    greet: () => {
-        console.log(this);
-        console.log(`Hey ${this.firstName}`)
-    }
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
 };
 jonas.greet();
 jonas.calcAge();
 
 // arguments keyword
 const addExpr = function (a, b) {
-    console.log(arguments);
-    return a + b;
+  console.log(arguments);
+  return a + b;
 };
 addExpr(2, 5);
 addExpr(2, 5, 8, 12);
 
 var addArrow = (a, b) => {
-    console.log(arguments);
-    return a + b;
+  console.log(arguments);
+  return a + b;
 };
 addArrow(2, 5, 8);
+*/
 
 
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+    name: 'Jonas',
+    age: 30,
+};
+const friend = me;
+friend.age = 27;
+console.log('Friend:', friend);
+console.log('Me', me);
