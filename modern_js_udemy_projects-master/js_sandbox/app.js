ebcb6a5ccdf07081e108c4e997c54f45b1aad5b4
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-26 09:59:23 +0200

    16. A Look At The Window Object

--- modern_js_udemy_projects-master/js_sandbox/app.js
+++ modern_js_udemy_projects-master/js_sandbox/app.js
@@ -617,7 +617,6 @@ todo.add();
 todo.edit(22);
 todo.delete();
 
-*/
 
 // 15. General Loops
 // FOR LOOP
@@ -694,3 +693,66 @@ const user = {
 for (let x in user) {
     console.log(`${x} : ${user[x]}`);
 }
+
+*/
+
+// 16. A Look At The Window Object
+// WINDOW METHODS / OBJECTS / PROPERTIES
+
+// Alert
+//alert('Hello World');
+
+// Prompt
+// const input = prompt();
+// alert(input);
+
+// Confirm
+// if(confirm('Are you sure')){
+//   console.log('YES');
+// } else {
+//   console.log('NO');
+// }
+
+let val;
+
+// Outter height and width
+val = window.outerHeight;
+val = window.outerWidth;
+
+// Inner height and width
+val = window.innerHeight;
+val = window.innerWidth;
+
+// Scroll points
+val = window.scrollY;
+val = window.scrollX;
+
+// Location Object
+val = window.location;
+val = window.location.hostname;
+val = window.location.port;
+val = window.location.href;
+val = window.location.search;
+
+// Redirect
+//window.location.href = 'http://google.com';
+//Reload
+//window.location.reload();
+
+// History Object
+
+// window.history.go(-2);
+// val = window.history.length;
+
+// Navigator Object
+val = window.navigator;
+val = window.navigator.appName;
+val = window.navigator.appVersion;
+val = window.navigator.userAgent;
+val = window.navigator.platform;
+val = window.navigator.vendor;
+val = window.navigator.language;
+
+
+
+console.log(val);
