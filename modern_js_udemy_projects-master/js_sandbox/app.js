93a4d81e80522d77fcbe08ec037e476873d6098b
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-26 08:55:30 +0200

    10. Object Literals

--- modern_js_udemy_projects-master/js_sandbox/app.js
+++ modern_js_udemy_projects-master/js_sandbox/app.js
@@ -293,7 +293,6 @@ html = `
 
 document.body.innerHTML = html;
 
-*/
 
 // 9. Arrays & Array Methods
 // Create some arrays
@@ -356,3 +355,45 @@ val = numbers.find(over50);
 
 console.log(numbers);
 console.log(val);
+
+*/
+
+// 10. Object Literals
+const person = {
+    firstName: 'Steve',
+    lastName: 'Smith',
+    age: 36,
+    email: 'steve@aol.com',
+    hobbies: ['music', 'sports'],
+    address: {
+        city: 'Miami',
+        state: 'FL'
+    },
+    getBirthYear: function () {
+        return 2017 - this.age;
+    }
+}
+
+let val;
+
+val = person;
+// Get specific value
+val = person.firstName;
+val = person['lastName'];
+val = person.age;
+val = person.hobbies[1];
+val = person.address.state;
+val = person.address['city'];
+val = person.getBirthYear();
+
+console.log(val);
+
+const people = [
+    { name: 'John', age: 30 },
+    { name: 'Mike', age: 23 },
+    { name: 'Nancy', age: 40 }
+];
+
+for (let i = 0; i < people.length; i++) {
+    console.log(people[i].name);
+}
