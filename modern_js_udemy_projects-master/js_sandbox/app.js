a31cb18743a8a61826d3681cc5e6071f2f287801
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-26 10:01:41 +0200

    17. Block Scope With let & const

--- modern_js_udemy_projects-master/js_sandbox/app.js
+++ modern_js_udemy_projects-master/js_sandbox/app.js
@@ -694,7 +694,6 @@ for (let x in user) {
     console.log(`${x} : ${user[x]}`);
 }
 
-*/
 
 // 16. A Look At The Window Object
 // WINDOW METHODS / OBJECTS / PROPERTIES
@@ -756,3 +755,34 @@ val = window.navigator.language;
 
 
 console.log(val);
+
+*/
+
+// 17. Block Scope With let & const
+// Global Scope
+var a = 1;
+let b = 2;
+const c = 3;
+
+// function test() {
+//   var a = 4;
+//   let b = 5;
+//   const c = 6;
+//   console.log('Function Scope: ', a, b, c);
+// }
+
+// test();
+
+// if(true) {
+//   // Block Scope
+//   var a = 4;
+//   let b = 5;
+//   const c = 6;
+//   console.log('If Scope: ', a, b, c);
+// }
+
+// for(var a = 0; a < 10; a++) {
+//   console.log(`Loop: ${a}`);
+// }
+
+console.log('Global Scope: ', a, b, c);
