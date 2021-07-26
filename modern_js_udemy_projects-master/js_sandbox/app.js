c87658c2e550c0d8c982cabbb943195f7baf570f
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-26 12:25:39 +0200

    11. Event Bubbling & Delegation

--- modern_js_udemy_projects-master/js_sandbox/app.js
+++ modern_js_udemy_projects-master/js_sandbox/app.js
@@ -1141,7 +1141,6 @@ function runEvent(e) {
     document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 40)`;
 }
 
-*/
 
 // 10. Keyboard & Input Events
 const form = document.querySelector('form');
@@ -1184,4 +1183,44 @@ function runEvent(e) {
     // console.log(taskInput.value);
 
     // e.preventDefault();
+}
+
+*/
+
+// 11. Event Bubbling & Delegation
+// EVENT BUBBLING
+
+// document.querySelector('.card-title').addEventListener('click', function(){
+//   console.log('card title');
+// });
+
+// document.querySelector('.card-content').addEventListener('click', function(){
+//   console.log('card content');
+// });
+
+// document.querySelector('.card').addEventListener('click', function(){
+//   console.log('card');
+// });
+
+// document.querySelector('.col').addEventListener('click', function(){
+//   console.log('col');
+// });
+
+// EVENT DELGATION
+
+// const delItem = document.querySelector('.delete-item');
+
+// delItem.addEventListener('click', deleteItem);
+
+document.body.addEventListener('click', deleteItem);
+
+function deleteItem(e) {
+    // if(e.target.parentElement.className === 'delete-item secondary-content'){
+    //   console.log('delete item');
+    // }
+
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        console.log('delete item');
+        e.target.parentElement.parentElement.remove();
+    }
 }
