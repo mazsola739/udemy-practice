37a49e7467fe50182613fae89ac43813abd9c419
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-26 14:07:35 +0200

    5. Using Object.create

--- modern-js-udemy-projects/starter-rika/00 js_sandbox/app.js
+++ modern-js-udemy-projects/starter-rika/00 js_sandbox/app.js
@@ -1396,7 +1396,6 @@ console.log(mary.getFullName());
 console.log(mary.hasOwnProperty('firstName'));
 console.log(mary.hasOwnProperty('getFullName'));
 
-*/
 
 // 4. Prototypal Inheritance
 // Person constructor
@@ -1439,3 +1438,34 @@ Customer.prototype.greeting = function () {
 }
 
 console.log(customer1.greeting());
+
+*/
+
+// 5. Using Object.create
+const personPrototypes = {
+    greeting: function () {
+        return `Hello there ${this.firstName} ${this.lastName}`;
+    },
+    getsMarried: function (newLastName) {
+        this.lastName = newLastName;
+    }
+}
+
+const mary = Object.create(personPrototypes);
+mary.firstName = 'Mary';
+mary.lastName = 'Williams';
+mary.age = 30;
+
+mary.getsMarried('Thompson');
+
+console.log(mary.greeting());
+
+const brad = Object.create(personPrototypes, {
+    firstName: { value: 'Brad' },
+    lastName: { value: 'Traversy' },
+    age: { value: 36 }
+});
+
+console.log(brad);
+
+console.log(brad.greeting());
