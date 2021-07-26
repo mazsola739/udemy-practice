36c848957e6199f5fa09d93b0f7b38b8d6105c26
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-26 09:51:11 +0200

    15. General Loops

--- modern_js_udemy_projects-master/js_sandbox/app.js
+++ modern_js_udemy_projects-master/js_sandbox/app.js
@@ -567,7 +567,6 @@ switch (new Date().getDay()) {
 
 console.log(`Today is ${day}`);
 
-*/
 
 // 14. Function Declarations & Expressions
 // FUNCTION DECLARATIONS
@@ -581,7 +580,7 @@ function greet(firstName = 'John', lastName = 'Doe') {
 
 // console.log(greet());
 
-// FUNCTION EXPRESIONS
+// FUNCTION EXPRESSIONS
 
 const square = function (x = 3) {
     return x * x;
@@ -617,3 +616,81 @@ todo.delete = function () {
 todo.add();
 todo.edit(22);
 todo.delete();
+
+*/
+
+// 15. General Loops
+// FOR LOOP
+
+// for(let i = 0; i < 10; i++){
+//   if(i === 2){
+//     console.log('2 is my favorite number');
+//     continue;
+//   }
+
+//   if(i === 5){
+//     console.log('Stop the loop');
+//     break;
+//   }
+
+//   console.log('Number '+ i);
+// }
+
+// WHILE LOOP
+
+// let i = 0;
+
+// while(i < 10){
+//   console.log('Number ' + i);
+//   i++;
+// }
+
+// DO WHILE
+
+// let i = 100;
+
+// do {
+//   console.log('Number ' + i);
+//   i++;
+// }
+
+// while(i < 10);
+
+// LOOP THROUGH ARRAY
+const cars = ['Ford', 'Chevy', 'Honda', 'Toyota'];
+
+// for(let i = 0; i < cars.length; i++){
+//   console.log(cars[i]);
+// }
+
+// FOREACH
+// cars.forEach(function(car, index, array){
+//   console.log(`${index} : ${car}`);
+//   console.log(array);
+// });
+
+// MAP
+// const users  = [
+//   {id: 1, name:'John'},
+//   {id: 2, name: 'Sara'},
+//   {id: 3, name: 'Karen'},
+//   {id: 4, name: 'Steve'}
+// ];
+
+// const ids = users.map(function(user){
+//   return user.id;
+// });
+
+// console.log(ids);
+
+
+// FOR IN LOOP
+const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 40
+}
+
+for (let x in user) {
+    console.log(`${x} : ${user[x]}`);
+}
