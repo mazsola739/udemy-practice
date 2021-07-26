329d6387cc839d6b2bde0895bae600b5cbc2711a
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-26 11:13:23 +0200

    6. Creating Elements

--- modern_js_udemy_projects-master/js_sandbox/app.js
+++ modern_js_udemy_projects-master/js_sandbox/app.js
@@ -925,7 +925,6 @@ for (let i = 0; i < liEven.length; i++) {
 
 console.log(items);
 
-*/
 
 // 5. Traversing The DOM
 let val;
@@ -982,3 +981,36 @@ val = listItem.nextElementSibling.nextElementSibling.previousElementSibling;
 val = listItem.previousSibling;
 val = listItem.previousElementSibling;
 console.log(val);
+
+*/
+
+// 6. Creating Elements
+// Create element
+const li = document.createElement('li');
+
+// Add class
+li.className = 'collection-item';
+
+// Add id
+li.id = 'new-item';
+
+// Add attribute
+li.setAttribute('title', 'New Item');
+
+// Create text node and append
+li.appendChild(document.createTextNode('Hello World'));
+
+// Create new link element
+const link = document.createElement('a');
+// Add classes
+link.className = 'delete-item secondary-content';
+// Add icon html
+link.innerHTML = '<i class="fa fa-remove"></i>';
+
+// Append link into li
+li.appendChild(link);
+
+// Append li as child to ul
+document.querySelector('ul.collection').appendChild(li);
+
+console.log(li);
