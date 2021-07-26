082e666f0ad43d46fbc9fbceeca4302d1510d6d9
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-26 13:47:37 +0200

    2. Built In Constructors

--- modern-js-udemy-projects/starter-rika/00 js_sandbox/app.js
+++ modern-js-udemy-projects/starter-rika/00 js_sandbox/app.js
@@ -1273,7 +1273,7 @@ tasks.forEach(function (task) {
 
 */
 
-/* */
+/*
 ////////////////////////////////////
 // 5. Object Oriented JavaScript - ES5 & ES2015
 
@@ -1297,3 +1297,52 @@ function Person(name, dob) {
 
 const brad = new Person('Brad', '9-10-1981');
 console.log(brad.calculateAge());
+
+*/
+
+// 2. Built In Constructors
+// String
+
+const name1 = 'Jeff';
+const name2 = new String('Jeff');
+
+//name2.foo = 'bar';
+// console.log(name2);
+
+console.log(typeof name2);
+
+if (name2 === 'Jeff') {
+    console.log('YES');
+} else {
+    console.log('NO');
+}
+
+// Number
+const num1 = 5;
+const num2 = new Number(5);
+
+// Boolean
+const bool1 = true;
+const bool2 = new Boolean(true);
+
+// Function
+const getSum1 = function (x, y) {
+    return x + y;
+}
+
+const getSum2 = new Function('x', 'y', 'return 1 + 1');
+
+// Object
+const john1 = { name: "John" };
+const john2 = new Object({ name: "John" });
+console.log(john2);
+
+// Arrays
+const arr1 = [1, 2, 3, 4];
+const arr2 = new Array(1, 2, 3, 4);
+
+// Regular Expressions
+const re1 = /\w+/;
+const re2 = new RegExp('\\w+');
+
+console.log(re2);
