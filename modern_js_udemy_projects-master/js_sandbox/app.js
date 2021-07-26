6ffc046a84a62c9459ff2d163cfea3aa60f070b3
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-26 08:17:09 +0200

    6. Numbers & The Math Object

--- modern_js_udemy_projects-master/js_sandbox/app.js
+++ modern_js_udemy_projects-master/js_sandbox/app.js
@@ -120,7 +120,6 @@ const today = new Date();
 console.log(today);
 console.log(typeof today);
 
-*/
 
 // 5. Type Conversion
 let val;
@@ -162,3 +161,34 @@ const sum = Number(val1 + val2);
 
 console.log(sum);
 console.log(typeof sum);
+
+*/
+
+// 6. Numbers & The Math Object
+const num1 = 100;
+const num2 = 50;
+let val;
+
+// Simple math with numbers
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 % num2;
+
+// Math Object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.abs(-3);
+val = Math.pow(8, 2);
+val = Math.min(2, 33, 4, 1, 55, 6, 3, -2);
+val = Math.max(2, 33, 4, 1, 55, 6, 3, -2);
+val = Math.random();
+
+val = Math.floor(Math.random() * 20 + 1);
+
+console.log(val);
