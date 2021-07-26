c10ca294b9cd4d87d1da9828bcfacf7399b3fa6d
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-26 07:59:08 +0200

    3. Variables - var, let & const

--- modern_js_udemy_projects-master/js_sandbox/app.js
+++ modern_js_udemy_projects-master/js_sandbox/app.js
@@ -1,11 +1,11 @@
+/*
 ////////////////////////////////////
 //2. JavaScript Language Fundamentals
 
 // 1. Section Intro & File Setup
-// alert('Hello World');
+alert('Hello World');
 
 // 2. Using The Console
-
 // Log to console
 
 console.log('Hello World');
@@ -29,6 +29,7 @@ console.log('Hello World');
 console.log('Hello World');
 console.log('Hello World');
 console.timeEnd('Hello');
+*/
 
 /*
 multi
@@ -36,3 +37,56 @@ line
 comments
 */
 
+// 3. Variables - var, let & const
+// var, let, const
+
+// var name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// // Init var
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
+
+// // letters, numbers, _, $
+// // Can not start with number
+
+// // Multi word vars
+// var firstName = 'John'; // Camel case
+// var first_name = 'Sara'; // Underscore
+// var FirstName = 'Tom'; // Pascal case
+// var firstname;
+
+// LET
+// let name;
+// name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+
+// CONST
+// const name = 'John';
+// console.log(name);
+// Can not reassign
+// name = 'Sara';
+// Have to assign a value
+// const greeting;
+
+const person = {
+    name: 'John',
+    age: 30
+}
+
+person.name = 'Sara';
+person.age = 32;
+
+// console.log(person);
+
+const numbers = [1, 2, 3, 4, 5];
+numbers.push(6);
+
+console.log(numbers);
