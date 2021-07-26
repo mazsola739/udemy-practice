2e0c2835f50d2ae74c830d5aea18c02d9a635dc4
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-26 10:56:09 +0200

    5. Traversing The DOM

--- modern_js_udemy_projects-master/js_sandbox/app.js
+++ modern_js_udemy_projects-master/js_sandbox/app.js
@@ -871,7 +871,6 @@ document.querySelector('li:nth-child(4)').textContent = 'Hello World';
 document.querySelector('li:nth-child(odd)').style.background = '#ccc';
 document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
 
-*/
 
 // 4. DOM Selectors For Multiple Elements
 // document.getElementsByClassName
@@ -925,3 +924,61 @@ for (let i = 0; i < liEven.length; i++) {
 
 
 console.log(items);
+
+*/
+
+// 5. Traversing The DOM
+let val;
+
+const list = document.querySelector('ul.collection');
+const listItem = document.querySelector('li.collection-item:first-child');
+
+val = listItem;
+val = list;
+
+// Get child nodes
+val = list.childNodes;
+val = list.childNodes[0];
+val = list.childNodes[0].nodeName;
+val = list.childNodes[3].nodeType;
+
+// 1 - Element
+// 2 - Attribute (deprecated)
+// 3 - Text node
+// 8 - Comment
+// 9 - Document itself
+// 10 - Doctype
+
+
+// Get children element nodes
+val = list.children;
+val = list.children[1];
+list.children[1].textContent = 'Hello';
+// Children of children
+list.children[3].children[0].id = 'test-link';
+val = list.children[3].children[0];
+
+// First child
+val = list.firstChild;
+val = list.firstElementChild;
+
+// Last child
+val = list.lastChild;
+val = list.lastElementChild;
+
+// Count child elements
+val = list.childElementCount;
+
+// Get parent node
+val = listItem.parentNode;
+val = listItem.parentElement;
+val = listItem.parentElement.parentElement;
+
+// Get next sibling
+val = listItem.nextSibling;
+val = listItem.nextElementSibling.nextElementSibling.previousElementSibling;
+
+// Get prev sibling
+val = listItem.previousSibling;
+val = listItem.previousElementSibling;
+console.log(val);
