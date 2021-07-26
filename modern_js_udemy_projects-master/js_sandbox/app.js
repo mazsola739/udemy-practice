e5066783c02f95acb7eaeca41fc3bc74047cc098
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-26 10:48:38 +0200

    3. DOM Selectors For Single Elements

--- modern_js_udemy_projects-master/js_sandbox/app.js
+++ modern_js_udemy_projects-master/js_sandbox/app.js
@@ -787,7 +787,7 @@ const c = 3;
 console.log('Global Scope: ', a, b, c);
 */
 
-/* */
+/*
 ////////////////////////////////////
 // 3. DOM Manipulation & Events
 
@@ -832,4 +832,43 @@ scriptsArr.forEach(function (script) {
 });
 
 console.log(val);
+
+*/
+
+// 3. DOM Selectors For Single Elements
+// document.getElementById()
+
+// console.log(document.getElementById('task-title'));
+
+// // Get things from the element
+// console.log(document.getElementById('task-title').id);
+// console.log(document.getElementById('task-title').className);
+
+// const taskTitle = document.getElementById('task-title');
+
+// // Change styling
+// taskTitle.style.background = '#333';
+// taskTitle.style.color = '#fff';
+// taskTitle.style.padding = '5px';
+// // taskTitle.style.display = 'none';
+
+// // Change content
+// taskTitle.textContent = 'Task List';
+// taskTitle.innerText = 'My Tasks';
+// taskTitle.innerHTML = '<span style="color:red">Task List</span>';
+
+// document.querySelector()
+
+console.log(document.querySelector('#task-title'));
+console.log(document.querySelector('.card-title'));
+console.log(document.querySelector('h5'));
+
+document.querySelector('li').style.color = 'red';
+document.querySelector('ul li').style.color = 'blue';
+
+document.querySelector('li:last-child').style.color = 'red';
+document.querySelector('li:nth-child(3)').style.color = 'yellow';
+document.querySelector('li:nth-child(4)').textContent = 'Hello World';
+document.querySelector('li:nth-child(odd)').style.background = '#ccc';
+document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
 
