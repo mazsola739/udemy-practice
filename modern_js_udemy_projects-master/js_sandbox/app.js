dde92b85e097956de832d23808deb90fa94f3279
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-26 07:47:22 +0200

    2. Using The Console

--- modern_js_udemy_projects-master/js_sandbox/app.js
+++ modern_js_udemy_projects-master/js_sandbox/app.js
@@ -1,7 +1,38 @@
-/////////////////////////
+////////////////////////////////////
 //2. JavaScript Language Fundamentals
 
 // 1. Section Intro & File Setup
-alert('Hello World');
+// alert('Hello World');
 
 // 2. Using The Console
+
+// Log to console
+
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1, 2, 3, 4]);
+console.log({ a: 1, b: 2 });
+console.table({ a: 1, b: 2 });
+
+console.error('This is some error');
+console.clear();
+console.warn('This is a warning');
+
+console.time('Hello');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.timeEnd('Hello');
+
+/*
+multi
+line
+comments
+*/
+
