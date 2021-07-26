ffdfac5c5e1599e0d0823bba52a80524408a896b
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-26 08:05:16 +0200

    4. Data Types in JavaScript

--- modern_js_udemy_projects-master/js_sandbox/app.js
+++ modern_js_udemy_projects-master/js_sandbox/app.js
@@ -37,7 +37,7 @@ line
 comments
 */
 
-
+/*
 // 3. Variables - var, let & const
 // var, let, const
 
@@ -91,3 +91,32 @@ const numbers = [1, 2, 3, 4, 5];
 numbers.push(6);
 
 console.log(numbers);
+*/
+
+// 4. Data Types in JavaScript
+// PRIMITIVE
+
+// String
+const name = 'John Doe';
+// Number
+const age = 45;
+// Boolean
+const hasKids = true;
+// Null
+const car = null;
+// Undefined
+let test;
+// Symbol
+const sym = Symbol();
+
+// REFERENCE TYPES - Objects
+// Array
+const hobbies = ['movies', 'music'];
+// Object literal
+const address = {
+    city: 'Boston',
+    state: 'MA'
+}
+const today = new Date();
+console.log(today);
+console.log(typeof today);
