cd26a1a76c56bdfa8ab7566ffd86fa9dd258e5b3
Erika Rostás <60824200+mazsola739@users.noreply.github.com>
2021-07-26 09:28:08 +0200

    14. Function Declarations & Expressions

--- modern_js_udemy_projects-master/js_sandbox/app.js
+++ modern_js_udemy_projects-master/js_sandbox/app.js
@@ -523,7 +523,6 @@ if (id === 100)
 else
     console.log('INCORRECT');
 
-    */
 
 // 13. Switches
 const color = 'yellow';
@@ -567,3 +566,54 @@ switch (new Date().getDay()) {
 }
 
 console.log(`Today is ${day}`);
+
+*/
+
+// 14. Function Declarations & Expressions
+// FUNCTION DECLARATIONS
+
+function greet(firstName = 'John', lastName = 'Doe') {
+    // if(typeof firstName === 'undefined'){firstName = 'John'}
+    // if(typeof lastName === 'undefined'){lastName = 'Doe'}
+    //console.log('Hello');
+    return 'Hello ' + firstName + ' ' + lastName;
+}
+
+// console.log(greet());
+
+// FUNCTION EXPRESIONS
+
+const square = function (x = 3) {
+    return x * x;
+};
+
+// console.log(square());
+
+// IMMIDIATLEY INVOKABLE FUNCTION EXPRESSIONS - IIFEs
+
+// (function(){
+//   console.log('IIFE Ran..');
+// })();
+
+// (function(name){
+//   console.log('Hello '+ name);
+// })('Brad');
+
+// PROPERTY METHODS
+
+const todo = {
+    add: function () {
+        console.log('Add todo..');
+    },
+    edit: function (id) {
+        console.log(`Edit todo ${id}`);
+    }
+}
+
+todo.delete = function () {
+    console.log('Delete todo...');
+}
+
+todo.add();
+todo.edit(22);
+todo.delete();
